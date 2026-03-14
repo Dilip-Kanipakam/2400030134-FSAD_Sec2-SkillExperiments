f9484d80016c2593fe673ffddbff4b6943d77042
Kanipakam Dilip Sai Nath Reddy <2400031034@kluniversity.in>
2026-03-14 17:59:57 +0530

    Skill 10 React Student Manager

---
+++ Skill Experiment -10/student-manager-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import StudentManager from "./StudentManager";
+
+function App() {
+  return (
+    <div>
+      <StudentManager />
+    </div>
+  );
+}
+
+export default App;
